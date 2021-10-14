59f3167292afe415eed42a75832b141983b68abb
rdp-jr <rdprincipio.jr@gmail.com>
2021-10-14 20:11:50 +0800

    test: add user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,9 +1,26 @@
 from fastapi.testclient import TestClient
 from fastapi import status
-
 from app.main import app
 
+client = TestClient(app)
+
 def test_create_user():
-    client = TestClient(app)
     response = client.post('/users', json={'name': 'testname'})
     assert response.status_code == status.HTTP_201_CREATED
+
+def test_get_user():
+    response = client.post('/users', json={'name': 'testname'})
+    response = response.json()
+    get_user = client.get(f"/users/{response['id']}")
+    get_user = get_user.json()
+    assert response['name'] == get_user['name']
+
+def test_update_user():
+    test_user = client.post('/users', json={'name': 'testname'})
+    test_user = test_user.json()
+    response = client.put(f"/users/{test_user['id']}", json={"name": "updated_name"})
+
+    get_user = client.get(f"/users/{test_user['id']}")
+    get_user = get_user.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert get_user['name'] == 'updated_name'

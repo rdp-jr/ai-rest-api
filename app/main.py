46bc6090fd5c51e27e1da1d6576e300de01210b5
rdp-jr <rdprincipio.jr@gmail.com>
2021-10-17 00:11:45 +0800

    feat: add cors middleware

--- app/main.py
+++ app/main.py
@@ -4,9 +4,19 @@ from app.routes.project import router as project_router
 from app.routes.real_dataset import router as real_dataset_router
 from app.routes.model import router as model_router
 from app.routes.synthetic_dataset import router as synthetic_dataset_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, tags=['users'])
 app.include_router(project_router, tags=['projects'])
